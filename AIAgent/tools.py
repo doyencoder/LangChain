63cd6d082ab8778f868d0b412c0ef87081316507
Rajat Bansal <bansalrajay730@gmail.com>
2026-01-15 18:10:42 +0530

    api key updated in .env

--- AIAgent/tools.py
+++ AIAgent/tools.py
@@ -1,18 +1,26 @@
-from langchain_core.tools import tool
+import os
+
+from dotenv import load_dotenv
 import requests
+from langchain_core.tools import tool
 from langchain_community.tools import DuckDuckGoSearchRun
+
+load_dotenv()
 
 search_tool = DuckDuckGoSearchRun()
 
 @tool
 def get_weather_data(city: str) -> str:
-  """
-  This function fetches the current weather data for a given city
-  """
-  url = f'https://api.weatherstack.com/current?access_key=62918e37cc14ebd45eaab80ea463cc21&query={city}'
+    """
+    This function fetches the current weather data for a given city
+    """
+    api_key = os.getenv("WEATHERSTACK_API_KEY")
+    if not api_key:
+        raise ValueError("WEATHERSTACK_API_KEY is not set")
 
-  response = requests.get(url)
+    url = f"https://api.weatherstack.com/current?access_key={api_key}&query={city}"
+    response = requests.get(url)
 
-  return response.json()
+    return response.json()
 
 tools = [search_tool, get_weather_data]
